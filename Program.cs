4d54573ffde56414dd1635c28f8c8696a20854bd
Devinz-craft <96115866+Devinz-craft@users.noreply.github.com>
2022-04-02 20:35:02 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello,my name is Obioha Daniel.I am in your backend class ");
+        Console.WriteLine("I want to beome a backend devloper because of different reasons but one of my early spark of intrest should be the fact that I was always fancinated about how th internet worked.");
+            
+        }
+    }
+}
